cb73d7fb2437be34f3132aa16c08e99486d24b81
JuanPablo <juanpablodiaz2518@gmail.com>
2025-08-30 01:04:19 -0400

    Nuevo mensaje del commit

--- resistor-color-trio/resistor-color-trio.ts
+++ resistor-color-trio/resistor-color-trio.ts
@@ -1,3 +1,32 @@
-export function decodedResistorValue() {
-  throw new Error('Remove this line and implement the function')
+export const COLORS = [ //array de colores 
+      'black',
+      'brown',
+      'red',
+      'orange',
+      'yellow', 
+      'green',
+      'blue',
+      'violet',
+      'grey',
+      'white',
+    ]
+
+export function decodedResistorValue(colores: Array<string>): string { //recibimos la variable de colores como un array de strings
+  const ohms = (COLORS.indexOf(colores[0]) * 10 + COLORS.indexOf(colores[1])) * 10 ** COLORS.indexOf(colores[2]); //retornamos el valor decodificado
+
+  if (ohms >= 0 && ohms <= 900) {
+    return `${ohms} ohms`
+  }
+  else if (ohms >= 1000 && ohms <= 900000) {
+    return `${ohms / 1000} kiloohms`
+  }
+  else if (ohms >= 1000000 && ohms <= 900000000) {
+    return `${ohms / 1000000} megaohms`
+  }
+  else if (ohms >= 1000000000) {
+    return `${ohms / 1000000000} gigaohms`
+  }
+  else {
+    throw new Error('Invalid color')
+  }
 }
